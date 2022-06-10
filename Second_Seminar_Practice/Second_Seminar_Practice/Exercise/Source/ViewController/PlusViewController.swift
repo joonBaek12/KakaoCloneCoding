5e17b02de845f649aefc2e3bfd02e6f72e028acf
Joon Baek <joonnbaek12@gmail.com>
2022-06-10 23:46:18 +0900

    {로그아웃버튼 추가}

--- Second_Seminar_Practice/Second_Seminar_Practice/Exercise/Source/ViewController/PlusViewController.swift
+++ Second_Seminar_Practice/Second_Seminar_Practice/Exercise/Source/ViewController/PlusViewController.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class PlusViewController: UIViewController {
 
+    //MARK: - LazyComponents
+    
+    
+    //MARK: - UIComponents
+    private let logoutButton = UIButton().then {
+        $0.setTitle("로그아웃", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 10, weight: .black)
+        $0.addTarget(self, action: #selector(touchupBackButton), for: .touchUpInside)
+    }
+    //MARK: - Variables
+
+
+    //MARK: - LifeCycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout()
+        view.backgroundColor = .white
+    }
+}
 
-        // Do any additional setup after loading the view.
+//MARK: - Extentions
+
+extension PlusViewController{
+    
+    //MARK: - LayoutHelpers
+    
+    private func layout() {
+        
+        view.adds(
+            [
+                touchupBackButton()
+            ]
+        )
+        touchupBackButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalTo(40)
+            $0.width.equalTo(100)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - GeneralHelpers
+        
+    // MARK: - ActionHelpers
+    
+    @objc
+    private func touchupBackButton() {
+       
+        self.navigationController?.navigationBar.isHidden = true//'<back버튼' 가리기
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
+//        welcomeVC.modalPresentationStyle = .overFullScreen
+//        self.present(welcomeVC, animated: true, completion: nil)
+//    }
 }
+
